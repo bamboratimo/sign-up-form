73f79b3c1da235200b1d63a9b0a4d639882e5c59
Timo Rautiainen <timo.rautiainen94@gmail.com>
2023-01-29 23:06:04 +0200

    add js for validating passwords and some styling

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 let pwdFields = document.querySelectorAll(".error");
+const notMatch = document.querySelector(".not-match-txt");
+
+pwdFields.forEach(pwd => {
+    pwd.classList.add("errorborder");
+});
+
+pwdFields.forEach(pwd => {
+    pwd.addEventListener("focusout", checkPwds2);
+});
+
+function checkPwds2(e) {
+    console.log(pwdFields[0].value, pwdFields[1].value)
+    if (pwdFields[0].value !== pwdFields[1].value || (pwdFields[0].value.length === 0 || pwdFields[1].value.length === 0)) {
+        pwdFields.forEach(pwd => {
+            pwd.classList.remove("valid");
+            pwd.classList.add("errorborder");
+            notMatch.style.visibility = "visible";
+        });
+    } else if (pwdFields[0].value === pwdFields[1].value) {
+        pwdFields.forEach(pwd => {
+            pwd.classList.remove("errorborder");
+            pwd.classList.add("valid");
+            notMatch.style.visibility = "hidden";
+        });
+    }
+}
